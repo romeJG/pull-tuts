5153920f92b88f0782df22495f6e610fc954d78c
romejg <justineguillermo00@gmail.com>
2023-03-27 18:15:07 +0800

    Added a comment from my local device.

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <!-- Hello i'm a commit from github or from my peers -->
+<!-- Hi im a comment from my local device - justine -->
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
